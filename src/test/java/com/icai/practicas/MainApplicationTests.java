295bb534ba48ea2cf753b694ac5867cab58097fb
javierjg001 <56931870+javierjg001@users.noreply.github.com>
2022-04-03 14:36:04 +0200

    Subida test DNI

--- src/test/java/com/icai/practicas/MainApplicationTests.java
+++ src/test/java/com/icai/practicas/MainApplicationTests.java
@@ -1,5 +1,9 @@
 package com.icai.practicas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.icai.practicas.model.DNI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,17 @@ class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		DNI dniTestTrue = new DNI("54364645M");
+		DNI dniTestFalse = new DNI("00000001R");
+
+		boolean resultadoDniTrue = dniTestTrue.validar();
+		boolean resultadoDniFalse = dniTestFalse.validar();
+
+		assertEquals(true, resultadoDniTrue);
+		assertEquals(false, resultadoDniFalse);
+
+
 	}
 
 }
